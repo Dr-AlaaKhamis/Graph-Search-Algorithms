9a3bb021a1e2e0d4e8be407f4bf0fc5ddf89aad3
omar-3 <s-omarkhaled@zewailcity.edu.eg>
2020-06-02 17:20:33 +0200

    first algorithm-ish

---
+++ utils.py
@@ -0,0 +1,10 @@
+from inspect import getsource
+from pygments.formatters import HtmlFormatter
+from pygments.lexers import PythonLexer
+from IPython.display import HTML
+from IPython.display import display
+from pygments import highlight
+
+def source(*functions):
+    source_code = '\n\n'.join(getsource(fn) for fn in functions)        
+    display(HTML(highlight(source_code, PythonLexer(), HtmlFormatter(full=True))))
