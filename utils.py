7e796675c627856faf331801163ad4e8ca462009
omar-3 <s-omarkhaled@zewailcity.edu.eg>
2020-06-09 19:20:55 +0200

    source function commented

--- utils.py
+++ utils.py
@@ -5,6 +5,11 @@ from IPython.display import HTML
 from IPython.display import display
 from pygments import highlight
 
+#
+# prints the source code of a list of functions
+# in the jupyter notebook
+#
+
 def source(*functions):
     source_code = '\n\n'.join(getsource(fn) for fn in functions)        
     display(HTML(highlight(source_code, PythonLexer(), HtmlFormatter(full=True))))
