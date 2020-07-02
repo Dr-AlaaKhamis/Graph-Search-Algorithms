ba89a19bcda0a9b709fb08fa379c1ac02de4ab9f
omar-3 <s-omarkhaled@zewailcity.edu.eg>
2020-07-02 13:08:05 +0200

    backend of graph drawing

--- utils.py
+++ utils.py
@@ -18,3 +18,42 @@ def source(*functions):
 # the following classes are meant for
 # drawing graphs and manipulate them on jupyter notebook
 #
+
+
+class Graph:
+    def __init__(self, graph_dict=None, directed=True):
+        self.graph_dict = graph_dict or {}
+        self.directed = directed
+        if not directed:
+            self.make_undirected()
+
+    def make_undirected(self):
+        for a in list(self.graph_dict.key()):
+            for (b, dist) in self.graph_dict[a].items():
+                self.connect1(b, a, dist)
+    
+    def connect(self, A, B, distance = 1):
+        self.connect1(A, B, distance)
+        if not self.directed:
+            self.connect1(B, A, distance)
+    
+    def connect1(self, A, B, distane):
+        self.graph_dict.setdefault(A, {})[B] = distance
+
+    def get(self, a, b=None):
+        links = self.graph_dict.setdefault(a, {})
+        if b is None:
+            return links
+        else:
+            return links.get(b)
+
+    def nodes(self):
+        s1 = set([k for k in self.graph_dict.keys()])
+        s2 = set([k2 for v in self.graph_dict.values() for k2, v2 in v.items()])
+        nodes = s1.union(s2)
+        return list(nodes)
+
+def UndirectedGraph(graph_dict=None):
+    return Graph(graph_dict=graph_dict, directed=False)
+
+    
