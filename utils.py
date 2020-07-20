fc4263b0840d87c6f211670058a4f11216993c12
omar-3 <s-omarkhaled@zewailcity.edu.eg>
2020-07-20 12:46:48 +0200

    added animation to GIS

--- utils.py
+++ utils.py
@@ -3,9 +3,11 @@ import networkx as nx
 import matplotlib.pyplot as plt
 from pygments.formatters import HtmlFormatter
 from pygments.lexers import PythonLexer
-from IPython.display import HTML
-from IPython.display import display
+from IPython.display import HTML, display,IFrame
 from pygments import highlight
+import matplotlib.animation as animation
+from matplotlib.collections import LineCollection
+from operator import attrgetter
 
 #
 # prints the source code of a list of functions
@@ -93,3 +95,39 @@ def getEdges(path):
     for i in range(length-1):
         edges.append((path[i], path[i+1]))
     return edges
+
+
+#
+# you give it colors steps of the animation and the graph 
+#
+
+def update_plot(i, data, scat):
+    scat.set_array(data[i])
+    return scat,
+
+def animate_simple(G, colors):
+    numframes = len(colors)
+    node_Xs = [float(x) for _, x in G.nodes(data='x')]
+    node_Ys = [float(y) for _, y in G.nodes(data='y')]
+    fig, ax =  plt.subplots(figsize=(15, 11))
+    ax.set_facecolor('w')
+    lines = []
+    for u, v, data in G.edges(keys=False, data=True):
+            if 'geometry' in data:
+                xs, ys = data['geometry'].xy
+                lines.append(list(zip(xs, ys)))
+            else:
+                x1 = G.nodes[u]['x']
+                y1 = G.nodes[u]['y']
+                x2 = G.nodes[v]['x']
+                y2 = G.nodes[v]['y']
+                line = [(x1, y1), (x2, y2)]
+                lines.append(line)
+
+    lc = LineCollection(lines, colors='#999999', linewidths=0.9, alpha=0.3)
+
+    ax.add_collection(lc)
+    scat = ax.scatter(node_Xs, node_Ys,c=colors[0], s=30)
+    ani = animation.FuncAnimation(fig, update_plot, frames=list(range(numframes)),
+                                 fargs = (colors, scat))
+    return ani
